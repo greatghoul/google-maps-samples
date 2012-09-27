7ee90d24810bc4a823a9d5526340e83b9bcf7970
greatghoul <greatghoul@gmail.com>
2012-09-27 22:07:35 +0800

    动态添加和减少停靠地点；修正自动完成地址，移除地址中的邮政编码

--- directions-wayspoints/main.js
+++ directions-wayspoints/main.js
@@ -7,6 +7,31 @@ var directionDisplay;
 var directionsService = new google.maps.DirectionsService();
 var map, geocoder;
 
+// 格式化字符串
+//
+// 用法：
+//
+// var s1 = '%{1} and %{2}!';
+// console.log('source: ' + s1);
+// console.log('target: ' + fmt(s1, 'ask', 'learn'));
+//
+// var s2 = "%{name} is %{age} years old, his son's name is %{sons[0].name}";
+// console.log('source: ' + s2);
+// console.log('target: ' + fmt(s2, { name: 'Lao Ming', age: 32, sons: [{ name: 'Xiao Ming' }]}));
+function fmt() {
+    var args = arguments;
+    return args[0].replace(/%\{(.*?)}/g, function(match, prop) {
+        return function(obj, props) {
+            var prop = /\d+/.test(props[0]) ? parseInt(props[0]) : props[0];
+            if (props.length > 1) {
+                return arguments.callee(obj[prop], props.slice(1));
+            } else {
+                return obj[prop] || '';
+            }
+        }(typeof args[1] === 'object' ? args[1] : args, prop.split(/\.|\[|\]\[|\]\./));
+    });
+}
+
 function initialize() {
     directionsDisplay = new google.maps.DirectionsRenderer();
     var chicago = new google.maps.LatLng(34.198564,108.895614);
@@ -56,20 +81,39 @@ function calcRoute() {
     });
 }
 
+var TPL_STOP = ''
+    + '<li class="location-wrap"><label>经过</label>'
+    + '  <input type="text" class="location" name="waypoint" />'
+    + '  <button type="button" class="close">×</button>'
+    + '</li>';
+
 $(function() {
-    $('.location').typeahead({
-        source: function (query, process) {
-            geocoder.geocode({ address: query }, function(results, status) {
-                var addressList = $.map(results, function(result) {
-                    return result.formatted_address;
+    function bind_typeahead($node) {
+        return $node.typeahead({
+            source: function (query, process) {
+                geocoder.geocode({ address: query }, function(results, status) {
+                    var addressList = $.map(results, function(result) {
+                        return result.formatted_address.replace(/\s*邮政编码\:.*/ig, '');
+                    });
+                    return process(addressList);
                 });
-                return process(addressList);
-            });
-        },
-        // matcher 返回 true 以支持带空格的模糊查询
-        matcher: function(item) {
-            return true; 
-        }
+            },
+
+            // matcher 返回 true 以支持带空格的模糊查询
+            matcher: function(item) {
+                return true; 
+            }
+        });
+    }
+    bind_typeahead($('.location'));
+
+    $('#add-stop-location').click(function() {
+        $stop = $(TPL_STOP); 
+        $('#stop-locations').append($stop);
+        bind_typeahead($stop.find('.location'));
+        $stop.find('.close').click(function() {
+            $(this).parents('.location-wrap').fadeOut('fast');
+        });
     });
 });
 
